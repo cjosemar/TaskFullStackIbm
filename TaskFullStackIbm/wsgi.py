5f6a988b4200bbf683228d84bbd50faf6ea96cc2
admin@pozo-alcon.com <admin@pozo-alcon.com>
2024-05-10 18:46:23 +0200

    Initial commit

---
+++ TaskFullStackIbm/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for TaskFullStackIbm project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TaskFullStackIbm.settings')
+
+application = get_wsgi_application()
